fac8f99271dcf8a5c5f668007c52b11d4b7c8cf2
Anyl Pascal <54882158+AnylPascal@users.noreply.github.com>
2020-07-15 13:17:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const firstOne = document.querySelector('.grid-triviaCards');
+firstOne.querySelector('.button1').innerText = 'HTML IS A PROGRAMING LANGUAGE';
+
+const secondOne = document.querySelector('.grid-triviaCards');
+secondOne.querySelector('.button2').innerText =
+	'HTML WAS INVENTED BY AN ANONYMOUS SOURCE';
+
+const thirdOne = document.querySelector('.grid-triviaCards');
+thirdOne.querySelector('.button3').innerText =
+	'JAVA AND JAVASCRIPT ARE THE SAME PROGRAMING LANGUAGE';
+
+const fourthOne = document.querySelector('.grid-triviaCards');
+fourthOne.querySelector('.button4').innerText =
+	'YOU CAN USE JAVASCRIPT TO APPLY CSS STYLINGS';
+
+const fifthOne = document.querySelector('.grid-triviaCards');
+fifthOne.querySelector('.button5').innerText =
+	'HTML WAS INVENTED BY TIM BERNERS-LESS ';
+
+const sixthOne = document.querySelector('.grid-triviaCards');
+sixthOne.querySelector('.button6').innerText =
+	'JAVASCRIPT IS A FRONT END SYNTAX LANGUAGE';
+
+const seventhOne = document.querySelector('.grid-triviaCards');
+seventhOne.querySelector('.button7').innerText =
+	'HTML CAN BE LOOKED AT LIKE THE BONES OF A WEBSITE';
+
+const eighthOne = document.querySelector('.grid-triviaCards');
+eighthOne.querySelector('.button8').innerText =
+	'HTML IS NOT A COMPUTER LANGUAGE';
+
+const ninthOne = document.querySelector('.grid-triviaCards');
+ninthOne.querySelector('.button9').innerText =
+	'CSS IS ONLY USED FOR THE STYLING OF A PAGE OR APP';
