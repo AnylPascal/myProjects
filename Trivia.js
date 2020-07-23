4ec027d00be1af3a0ddf07f7c13b2924ef17b3df
AnylPascal <info@anylpascal.com>
2020-07-22 21:45:47 -0400

    added React, Node.js

---
+++ Trivia.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+// class Trivia extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   handleClick = () => {
+//     fetch(
+//       `https://official-joke-api.appspot.com/random_joke${this.state.handleClick}`
+//     )
+//       .then(res => res.json())
+//       .then(json => {
+//         this.setState({});
+//       });
+//   };
+//   render() {
+//     return (
+//       <p>{this.state.data.setup}</p>
+//       <li onClick={this.state.data.punchline}>
+        
+//       </li>
+//     );
+//   }
+// }
+// export default Joke;
